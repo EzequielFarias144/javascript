23a2b96327650703d53b5469058be48bffc7c81b
ezequielfarias144 <ezequielfarias144@gmail.com>
2024-09-10 11:44:17 -0300

    tailwind

--- facebook/src/App.js
+++ facebook/src/App.js
@@ -1,18 +1,68 @@
-import './App.css';
+import "./App.css"
 import Form from './formulario';
 
 function App(props) {
   return (
     <div className="App">
+      <div className="bg-blue-500 text-white p-4">
+      <h1 className="text-3xl font-bold">Olá, Tailwind CSS!</h1>
+      <p>Este é um exemplo simples de como usar Tailwind no React.</p>
+    </div>
+
      <header>
-       <h1>Facebook</h1>
+       <img src="./face-foto.svg" alt="facebook" />
        <p>O Facebook ajuda você a se conectar e compartilhar com as pessoas que fazem parte da sua vida.</p>
      </header>
      
      <Form></Form>
-      <footer>
-  
-    </footer>
+        <footer>
+        <ul>
+          <li>Português (Brasil)</li>
+          <li>English (US)</li>
+          <li>Español</li>
+          <li>Français (France)</li>
+          <li>Italiano</li>
+          <li>Deutsch</li>
+          <li>العربية</li>
+          <li>हिन्दी</li>
+          <li>中文(简体)</li>
+          <li>日本語</li>
+          <li>Cadastre-se</li>
+          <li>Entrar</li>
+          <li>Messenger</li>
+          <li>Facebook Lite</li>
+          <li>Vídeo</li>
+          <li>Locais</li>
+          <li>Jogos</li>
+          <li>Marketplace</li>
+          <li>Meta Pay</li>
+          <li>Meta Store</li>
+          <li>Meta Quest</li>
+          <li>Ray-Ban Meta</li>
+          <li>Meta AI</li>
+          <li>Instagram</li>
+          <li>Threads</li>
+          <li>Campanhas de arrecadação de fundos</li>
+          <li>Serviços</li>
+          <li>Central de Informações de Votação</li>
+          <li>Política de Privacidade</li>
+          <li>Central de Privacidade</li>
+          <li>Grupos</li>
+          <li>Sobre</li>
+          <li>Criar anúncio</li>
+          <li>Criar Página</li>
+          <li>Desenvolvedores</li>
+          <li>Carreiras</li>
+          <li>Cookies</li>
+          <li>Escolhas para anúncios</li>
+          <li>Termos</li>
+          <li>Ajuda</li>
+          <li>Carregamento de contatos e não usuários</li>
+          <li>Configurações</li>
+          <li>Registro de atividades</li>
+        </ul>
+        <p>Meta  2024</p>
+      </footer>
     </div>
   );
 }
--- facebook/src/formulario.js
+++ facebook/src/formulario.js
@@ -1,11 +1,12 @@
 const Form = (props) => {
     return (
         <div className="form">
-            <input type="email" placeholder="Email ou telefone"></input>
-            <input type="password"></input>
-            <button className="entrar">Entrar</button> 
-            <a href="#">Esqueceu a senha?</a>
-            <button className="conta">Criar nova conta</button>
+            <input type="email" placeholder="Email ou telefone"></input><br />
+            <input type="password"></input><br />
+            <button className="entrar">Entrar</button> <br />
+            <a href="#">Esqueceu a senha?</a><br />
+            <a href="#">Criar nova conta</a><br />
+            <p><a href="#">Crie uma Página</a> para uma celebridade, uma marca ou uma emprese.</p>
         </div>
     );
 };
