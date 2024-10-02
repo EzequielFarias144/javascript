105096b4b2422571c491e21b171767a8d154e53d
ezequielfarias144 <ezequielfarias144@gmail.com>
2024-10-02 14:14:28 -0300

    face

---
+++ facebook2/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="text-center mt-5">Bem-vindo ao React com Bootstrap!</h1>
+      <button className="btn btn-primary  justify-center">Clique aqui</button>
+    </div>
+  );
+}
+
+export default App;
