6031e30ecb87dac46cc0f1ea1220f0e66dcda192
Ezequiel Elisel Farias Carrizo <ezequielfarias144@gmail.com>
2025-05-10 20:57:49 -0300

    Atualizar o atividade-04.js

--- ifb-modulo-2-web/aula-04/atividade-04.js
+++ ifb-modulo-2-web/aula-04/atividade-04.js
@@ -2,7 +2,6 @@ function obterDescricao(livro) {
     return `${livro.titulo}, autor: ${livro.autor}, ${livro.paginas} páginas`;
 }
 
-// Criando dois objetos livro diferentes
 const livro1 = {
     titulo: "Dom Casmurro",
     autor: "Machado de Assis",
@@ -15,6 +14,5 @@ const livro2 = {
     paginas: 328
 };
 
-// Testando a função com os dois livros
 console.log(obterDescricao(livro1));
 console.log(obterDescricao(livro2));
